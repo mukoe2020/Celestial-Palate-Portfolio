9c75309a3e928fde823516c71e85a7a1d34f30f6
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-02-05 14:04:37 +0200

    reservations fixing

--- app/routes/reservations.py
+++ app/routes/reservations.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, jsonify, request, Blueprint
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,18 +10,23 @@ app = Flask(__name__)
 
 reservation = Blueprint('reservation', __name__)
 
-@reservation.route('/', methods=['POST'])
-def create_reservation() -> Response:
+@reservation.route('/customer_id',methods=['POST'])
+def create_reservation(customer_id):
     if request.method == 'POST':
         session = DBSession()
-        # Assuming request contains customer_id, payment_id, and num_of_guests
-        new_reservation = Reservation(customer_id=request.json['customer_id'], payment_id=request.json['payment_id'], num_of_guests=request.json['num_of_guests'],
-                                      created_at=datetime.utcnow(), updated_at=datetime.utcnow())
+        new_reservation = Reservation(
+            customer_id=customer_id,
+            payment_id=request.json['payment_id'],
+            num_of_guests=request.json['num_of_guests'],
+            created_at=datetime.utcnow(),
+            updated_at=datetime.utcnow()
+        )
         session.add(new_reservation)
         session.commit()
         session.close()
         return jsonify(message="Reservation created successfully")
-    
+
+
 @reservation.route('/<reservation_id>', methods=['GET'])
 def get_reservation(reservation_id):
     session = DBSession()
@@ -30,6 +36,8 @@ def get_reservation(reservation_id):
         return jsonify(reservation={'id': reservation.id, 'customer_id': reservation.customer_id, 'payment_id': reservation.payment_id, 'num_of_guests': reservation.num_of_guests, 'created_at': reservation.created_at, 'updated_at': reservation.updated_at})
     else:
         return jsonify(message="Reservation not found"), 404
+
+    
     
 @reservation.route('/<reservation_id>', methods=['DELETE'])
 def delete_reservation(reservation_id):
