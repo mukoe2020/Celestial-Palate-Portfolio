655540ac57d7f7a12429c72aa5d8e795a6eeb924
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-02-12 23:59:05 +0200

    new branch

--- app/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-class Config:
-
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://habiba:babo@localhost:3306/celestial'
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/database.py
+++
@@ -1,8 +0,0 @@
-# database.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
-connection_string = 'mysql+mysqlconnector://habiba:babo@localhost:3306/celestial'
-engine = create_engine(connection_string, pool_pre_ping=True)
-DBSession = sessionmaker(bind=engine)
