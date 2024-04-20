47f2803b5c06ecbb3fe4ac90bc7488c14a58da3c
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-20 14:55:25 +0200

    adding data

--- v_2/datab.py
+++ v_2/datab.py
@@ -10,7 +10,7 @@ from pymongo.server_api import ServerApi
 user_name = os.environ.get('MONGO_USER')
 passw= os.environ.get('MONGO_PASS')
 
-uri = f"mongodb+srv://{user_name}:{passw}@celestial.qtekhwe.mongodb.net/?retryWrites=true&w=majority&appName=Celestial"
+uri = f"mongodb+srv://{username}:{password}@celestial.qtekhwe.mongodb.net/?retryWrites=true&w=majority&appName=Celestial"
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 database = client['celestial_db']
@@ -31,7 +31,31 @@ first_user_data = {
     "updated_at": datetime.now()
 }
 
+second_user_data = {
+    "first_name": "kudzie",
+    "last_name":  "musekwa",
+    "email":    "kukumus@gmail.com",
+    "phone": "0987654321",
+    "created_at": datetime.now(),
+    "updated_at": datetime.now()
+}
+
+third_user_data = {
+    "first_name": "salem",
+    "last_name": "duke",
+    "email": "salemdue@gmail.com",
+    "phone": "4569874501",
+    "created_at": datetime.now(),
+    "updated_at": datetime.now()
+}
+#inserting data into the collections
 input = customer_col.insert_one(first_user_data)
+customer_id = input.inserted_id
+
+input = customer_col.insert_one(second_user_data)
+customer_id = input.inserted_id
+
+input = customer_col.insert_one(third_user_data)
 customer_id = input.inserted_id
 
 first_payment = {
@@ -41,8 +65,34 @@ first_payment = {
     "created_at": datetime.now(),
     "updated_at": datetime.now()
 }
+
+second_payment = {
+    "customer_id" : customer_id,
+    "amount" : 200,
+    "status": "completed",
+    "created_at": datetime.now(),
+    "updated_at": datetime.now()
+}
+
+third_payment = {
+    "customer_id" : customer_id,
+    "amount" : 300,
+    "status": "completed",
+    "created_at": datetime.now(),
+    "updated_at": datetime.now()
+}
+
+#innserting data into the payment collection
 p_input = payment_col.insert_one(first_payment)
 payment_id = p_input.inserted_id
+
+p_input = payment_col.insert_one(second_payment)
+payment_id = p_input.inserted_id
+
+p_input = payment_col.insert_one(third_payment)
+payment_id = p_input.inserted_id
+
+
 
 first_reservation = {
     "customer_id" : customer_id,
@@ -51,8 +101,26 @@ first_reservation = {
     "created_at": datetime.now(),
     "updated_at": datetime.now()
 }
-reservation_col.insert_one(first_reservation)
 
+second_reservation = {
+    "customer_id" : customer_id,
+    "payment_id": payment_id,
+    "num_of_guests": 4,
+    "created_at": datetime.now(),
+    "updated_at": datetime.now()
+}
+
+third_reservation = {
+    "customer_id" : customer_id,
+    "payment_id": payment_id,
+    "num_of_guests": 6,
+    "created_at": datetime.now(),
+    "updated_at": datetime.now()
+}
+#inserting data into the reservation collection
+reservation_col.insert_one(first_reservation)
+reservation_col.insert_one(second_reservation)
+reservation_col.insert_one(third_reservation)
 
 """
 try:
