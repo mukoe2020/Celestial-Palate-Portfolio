096f330c38f8902c97252219e76baa46be7a339d
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-28 17:41:39 +0200

    payment and reservation endpoits files created

---
+++ v_2/rest/app/all_routes/reserv.py
@@ -0,0 +1,55 @@
+from flask import jsonify, abort, request
+from flask.blueprints import Blueprint
+from pymongo import MongoClient
+from bson import ObjectId
+from datetime import datetime
+
+mongo_reservations = Blueprint('mongo_reservations', __name__)
+
+@mongo_reservations.route('/', methods=['GET'], strict_slashes=False)
+def get_reservations():
+    from v_2.rest.app import client
+    database = client['celestial_db']
+    collection = database['Reservations']
+    all_docs = collection.find()
+
+    """converting the ObjectId to string for jsonify"""
+    reservations = []
+    for doc in all_docs:
+        doc['_id'] = str(doc['_id'])
+        reservations.append(doc)
+    return jsonify(list(reservations)), 200
+
+@mongo_reservations.route('/<string:reservation_id>', methods=['GET'], strict_slashes=False)
+def get_reservation(reservation_id):
+    from v_2.rest.app import client
+    database = client['celestial_db']
+    collection = database['Reservations']
+    reservation = collection.find_one({'_id': ObjectId(reservation_id)})
+    if reservation:
+        reservation['_id'] = str(reservation['_id'])
+        return jsonify(reservation), 200
+    else:
+        abort(404)
+
+@mongo_reservations.route('/', methods=['POST'], strict_slashes=False)
+def create_reservation():
+    """creates a new reservation"""
+    from v_2.rest.app import client
+    database = client['celestial_db']
+    collection = database['Reservations']
+    if not request.json:
+        abort(400)
+    if 'payment_id' not in request.json or 'customer_id' not in request.json \
+        or 'num_of_guest' not in request.json:
+        abort(400)
+    reservation = { 'payment_id': request.json['payment_id'],
+                    'customer_id': request.json['customer_id'],
+                    'num_of_guest': request.json['num_of_guest'],
+                    'created_at' : datetime.now(),
+                    'updated_at' : datetime.now()
+                }
+    result = collection.insert_one(reservation)
+    # convert the ObjectId to string for jsonify
+    reservation['_id'] = str(result.inserted_id)
+    return jsonify(reservation), 201
